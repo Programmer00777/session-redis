07a490b3d48dbc77f4f1fbccbf4f704ea7f64c26
Programmer00777 <55809302+Programmer00777@users.noreply.github.com>
2022-11-15 12:56:03 +0200

    initial commit

---
+++ src/test/java/com/chernikov/sessionredis/SessionRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chernikov.sessionredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
